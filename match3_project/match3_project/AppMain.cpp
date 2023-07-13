b6da6b652b43e79696984d534b9cb51c580ac0ee
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2023-07-13 12:10:00 +0900

    AppMainの中身を追加
    AppMain.pdfの中身を写しつつ書いた

--- match3_project/match3_project/AppMain.cpp
+++ match3_project/match3_project/AppMain.cpp
@@ -1,21 +1,45 @@
+/*********************************
+プログラミング実習
+マッチ3ゲーム制作
+*********************************/
 #include "DxLib.h"
+#include "FreamControl.h"
+#include "InputControl.h"
+#include "SceneManager.h"
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR IpCmdLine, _In_ int nShowCmd)
+
+/*********************************
+マクロ定義
+*********************************/
+#define SCREEN_HEIGHT (480)		//スクリーンサイズ(高さ)
+#define SCREEN_WIDTH (640)		//スクリーンサイズ(幅)
+#define SCREEN_COLORBIT (32)	//スクリーンカラービット
+#define FONT_SIZE (20)			//文字サイズ
+
+/*********************************
+型定義
+*********************************/
+
+
+/*********************************
+グローバル変数宣言
+*********************************/
+
+
+/*********************************
+プロトタイプ宣言
+*********************************/
+
+/*********************************
+プログラムの開始
+*********************************/
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
+
+	//ウィンドタイトル設定
+	SetMainWindowText("Match 3 Puzzle");
+
 	//ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
-
-	//Dxライブラリの初期化処理
-	if (DxLib_Init() == -1)
-	{
-		return -1;
-	}
-
-	//入力待ち
-	WaitKey();
-
-	//Dxライブラリ使用の終了処理
-	DxLib_End();
-
-	return 0;
 }
