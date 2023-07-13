372144e6c769ead02d6085060538bbfe13b9365b
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2023-07-13 15:00:18 +0900

    マッチ3の中身を書いた
    AppMainは終わった
    フレームコントロールは終わった
    インプットコントロールは書いてる途中

--- match3_project/match3_project/AppMain.cpp
+++ match3_project/match3_project/AppMain.cpp
@@ -30,6 +30,7 @@
 プロトタイプ宣言
 *********************************/
 
+
 /*********************************
 プログラムの開始
 *********************************/
@@ -42,4 +43,57 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 	//ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
+
+	//画面サイズの最大サイズ,カラービット数を設定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
+
+	//Dxライブラリ初期化処理
+	//エラーが発生したら、終了する
+	if (DxLib_Init() == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//各機能の初期化処理
+	FreamControl_Initialize();	//フレームレート制御機能
+	Input_Initialize();			//入力制御機能
+	//シーンマネージャー初期化処理
+	//エラーが発生したら、終了する
+	if (SceneManager_Initialize(E_TITLE) == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//描画先画面を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定
+	SetFontSize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessage() != D_ERROR && Input_Escape() == FALSE)
+	{
+		//入力制御機能更新処理
+		Input_Update();
+
+		//シーンマネージャー更新処理
+		SceneManager_Update();
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//シーンマネージャー描画処理
+		SceneManeger_Draw();
+
+		//フレームレート制御処理
+		FreamControl_Update();
+
+		//画面の内容を表画面に反映
+		ScreenFlip();
+	}
+
+	//Dxライブラリ使用の終了処理
+	DxLib_End();
+
+	return 0;
 }
--- match3_project/match3_project/InputControl.h
+++ match3_project/match3_project/InputControl.h
@@ -1,1 +1,23 @@
 #pragma once
+/*********************************
+マクロ定義
+*********************************/
+
+
+/*********************************
+型定義
+*********************************/
+
+
+/*********************************
+プロトタイプ宣言
+*********************************/
+void Input_Initialize(void);	//入力初期化
+void Input_Update(void);		//入力更新
+int Input_Escape(void);			//エスケープキーの入力
+
+int GetOldKey(int key);			//過去押されていたキー
+int GetNowKey(int key);			//現在押されているキー
+int GetKeyFlg(int key);			//キーが押されているか
+int GetMousePositionX(int key);	//マウスポイントのX
+int GetMousePositionY(int key);	//マウスポイントのY
