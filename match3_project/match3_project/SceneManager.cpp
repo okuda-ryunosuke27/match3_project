dd1f864c782f3ab23b6bdddbedbe60cdb1f5115f
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2023-07-20 10:06:59 +0900

    シーンマネージャー追加
    初期化処理と更新処理完成
    描画処理は最初だけ書いた

--- match3_project/match3_project/SceneManager.cpp
+++ match3_project/match3_project/SceneManager.cpp
@@ -48,5 +48,60 @@ int SceneManager_Initialize(GAME_MODE mode)
 	}
 
 	//ゲームクリア画面
+	Read_Error = GameClearScene_Initialize();
+	if (Read_Error == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//ゲームオーバー画面
+	Read_Error = GameOverScene_Initialize();
+	if (Read_Error == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	Game_Mode = mode;
+	Next_Mode = Game_Mode;
+
+	return Read_Error;
 }
 
+/*********************************
+シーン管理機能：更新処理
+引　数：なし
+戻り値：なし
+*********************************/
+void SceneManager_Update(void)
+{
+	//前フレームとゲームモードが違っていたらシーンを切り替える
+	if (Game_Mode != Next_Mode)
+	{
+		SceneManager_Initialize(Next_Mode);
+	}
+
+	//各画面の更新処理
+	switch (Game_Mode)
+	{
+		case E_TITLE:
+			TitleScene_Update();
+			break;
+		case E_GAMEMAIN:
+			GameMainScene_Update();
+			break;
+		case E_GAME_CLEAR:
+			GameClearScene_Update();
+			break;
+		case E_GAME_OVER:
+			GameOverScene_Update();
+			break;
+		default:
+			break;
+	}
+}
+
+/*********************************
+シーン管理機能：描画処理
+引　数：なし
+戻り値：なし
+*********************************/
