81b288113da0972c764e9f66267dcbcbb35134f5
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2023-08-10 19:38:40 +0900

    gamemain
    gamemainかけた
    しかし挙動がおかしい
    マウスが縦に選択できないので修正しないといけない

--- match3_project/match3_project/GameMainScene.cpp
+++ match3_project/match3_project/GameMainScene.cpp
@@ -120,5 +120,36 @@ void GameMainScene_Update()
 *********************************/
 void GameMainScene_Draw()
 {
+	int PosX = 600;
+	int tmp_level = GameLevel;
+	int tmp_score = Get_StageScore();
 
+	//ステージを描画
+	StageDraw();
+
+	//フェードアウト状態か?
+	if (Get_StageState() == 1)
+	{
+		FadeOutBlock();		//フェードアウトする
+	}
+
+	//レベルを描画
+	do
+	{
+		DrawRotaGraph(PosX, 80, 0.5f, 0, NumberImage[tmp_level % 10], TRUE);
+		tmp_level /= 10;
+		PosX -= 30;
+	} while (tmp_level > 0);
+
+	//スコアの描画
+	PosX = 620;
+	do
+	{
+		DrawRotaGraph(PosX, 160, 0.3f, 0, NumberImage[tmp_score % 10], TRUE);
+		tmp_score /= 10;
+		PosX -= 20;
+	} while (tmp_score > 0);
+
+	//制限時間の描画
+	DrawBox(491, 469, 509, 469 - GameTime / 60 * 2, 0x0033ff, TRUE);
 }
